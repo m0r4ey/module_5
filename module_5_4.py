2e09426f1094c68568518bc034cf15b74a3aeeb3
m0rfey <plaksinaleks2306@mail.ru>
2024-11-17 15:24:41 +0500

    Различие атрибутов класса и экземпляра

---
+++ module_5_4.py
@@ -0,0 +1,90 @@
+# Различие атрибутов класса и экземпляра
+
+class House:
+
+    houses_history =[]
+
+    def __new__(cls, *args, **kwargs):
+        # Создаем новый объект
+        instance = super(House, cls).__new__(cls)
+        # Добавляем имя дома в историю
+        if args:
+            cls.houses_history.insert(0, args[0])
+        elif 'name' in kwargs:
+            cls.houses_history.insert(0, kwargs['name'])
+        return instance
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < self.number_of_floors:
+            for i in range(new_floor):
+                print(i+1)
+        else:
+            print('Такого этажа не существует')
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
